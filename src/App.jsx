38b350f6644cb7f1fcee71e60e0bc131cb9ef72c
ian <ianmeigh@googlemail.com>
2022-12-13 17:37:43 +0000

    feat: add user filtering

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,15 @@ export class App extends Component {
     super();
     this.state = {
       users: [],
+      searchQuery: "",
+      isDataLoaded: false,
     };
   }
+
+  handleInputChange = (e) => {
+    const searchQuery = e.target.value.toLocaleLowerCase();
+    this.setState({ searchQuery });
+  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -17,25 +24,42 @@ export class App extends Component {
         }
         return response.json();
       })
-      .then((users) => this.setState(() => ({ users })))
+      .then((users) => this.setState({ users, isDataLoaded: true }))
       .catch((err) => console.log(`Problem communicating with API, ${err}`));
   }
 
   render() {
+    const { users, searchQuery, isDataLoaded } = this.state;
+    const { handleInputChange } = this;
+
+    const filteredUsers = users.filter((user) =>
+      user.name.toLocaleLowerCase().includes(searchQuery)
+    );
+
     return (
       <div className="App">
         <h1>Company Directory</h1>
+        <input
+          type="search"
+          placeholder="Search Directory..."
+          onChange={handleInputChange}
+        />
         <div className="CardsContainer">
-          {this.state.users.length === 0 ? (
-            <p>Loading...</p>
+          {/* Display Data */}
+          {isDataLoaded ? (
+            filteredUsers.length === 0 ? (
+              <p>No Results</p>
+            ) : (
+              filteredUsers.map((user) => {
+                return (
+                  <div className="Card" key={user.id}>
+                    <p>{user.name}</p>
+                  </div>
+                );
+              })
+            )
           ) : (
-            this.state.users.map((user) => {
-              return (
-                <div className="Card" key={user.id}>
-                  <p>{user.name}</p>
-                </div>
-              );
-            })
+            <p>Loading...</p>
           )}
         </div>
       </div>
