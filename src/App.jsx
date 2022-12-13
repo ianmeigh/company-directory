869424d053c4d93b247418b04a394468a8737865
ian <ianmeigh@googlemail.com>
2022-12-13 16:54:15 +0000

    feat: fetch data from api and render

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
 import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <header>Application Placeholder</header>
-    </div>
-  );
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then((users) => this.setState(() => ({ users })))
+      .catch((err) => console.log(`Problem communicating with API, ${err}`));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Company Directory</h1>
+        <div className="CardsContainer">
+          {this.state.users.length === 0 ? (
+            <p>Loading...</p>
+          ) : (
+            this.state.users.map((user) => {
+              return (
+                <div className="Card" key={user.id}>
+                  <p>{user.name}</p>
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
